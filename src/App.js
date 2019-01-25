d40cb3fd41a816f8baa5ae8a75423204024c91a4
Ipul Saepulloh <ipulsaepulloh9@gmail.com>
2019-01-25 15:48:17 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Number from "./components/Number";
+import Counter from "./components/Counter";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Number />
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Number.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux"
+
+const Number = ({ number }) => {
+  return <h1>{number}</h1>;
+};
+
+const mapStateToProps = store => ({
+  number: store.counter.number
+})
+
+export default connect(mapStateToProps)(Number);
